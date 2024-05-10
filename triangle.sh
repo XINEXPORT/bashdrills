ca0d6c27c6c4b0709e74232b00aa023f285fa712
Christine Hoang <christinehoangt@gmail.com>
2024-05-10 12:33:08 -0500

    triangle comparison

---
+++ triangle.sh
@@ -0,0 +1,12 @@
+read x
+read y
+read z
+
+if [ $x == $y -a $y == $z -a $x == $z ]; then
+ echo 'EQUILATERAL'
+elif [ $x == $y -o $y == $z -o $x == $z ]; then
+ echo 'ISOCELES'
+else
+ echo 'SCALENE'
+
+ fi
