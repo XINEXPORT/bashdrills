ffd00bc40fd2fb7b120918337b34127a2281948b
Christine Hoang <christinehoangt@gmail.com>
2024-05-10 15:51:38 -0500

    ciao

---
+++ ciao.sh
@@ -0,0 +1,5 @@
+i=3; while [ $i -gt 0 ]; do echo $i; let i-=1; done
+
+for m in hi salut ciao; do echo $m; done
+
+
--- triangle.sh
+++ triangle.sh
@@ -2,11 +2,10 @@ read x
 read y
 read z
 
-if [ $x == $y -a $y == $z -a $x == $z ]; then
+if [ $x == $y -a $y == $z -a $x == $z]; then
  echo 'EQUILATERAL'
-elif [ $x == $y -o $y == $z -o $x == $z ]; then
+elif [ $x == $y -o $y == $z -o $x == $z]; then
  echo 'ISOCELES'
 else
  echo 'SCALENE'
-
- fi
+fi
